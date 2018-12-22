bff2f2a603eb46dbd6867518f12d6853c836d456
yangdepp <965093975@qq.com>
2018-12-22 15:36:19 +0800

    init

---
+++ main.js
@@ -0,0 +1,26 @@
+initCarouselItem();
+
+setInterval(() => {
+  $(`.images>img:nth-child(${setN(n)})`).removeClass('current enter').addClass('leave')
+    .one('transitionend', (e) => {
+      $(e.currentTarget).removeClass('leave current').addClass('enter');
+    })
+  $(`.images>img:nth-child(${setN(n + 1)})`).removeClass('enter leave').addClass('current')
+  n += 1
+}, 1500)
+
+function setN(n) {
+  if (n > 3) {
+    n = n % 3
+    if (n === 0) {
+      n = 3
+    }
+  }
+  return n
+}
+
+function initCarouselItem() {
+  n = 1
+  $(`.images>img:nth-child(${n}`).addClass('current')
+    .siblings().addClass('enter')
+}
